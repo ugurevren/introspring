9d57133369bde53aa18ee6c16e6447b77954568c
Uğur Evren Çamalan <ugurevrencamalan@gmail.com>
2024-12-16 21:40:48 +0300

    Initial commit

---
+++ introspring/src/main/java/com/ugurevren/introspring/IntrospringApplication.java
@@ -0,0 +1,13 @@
+package com.ugurevren.introspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrospringApplication.class, args);
+	}
+
+}
